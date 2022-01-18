642d980b3a8e59a3e33ba90285cec2417bfc1157
bachngocng <nnbach97@gmail.com>
2022-01-19 00:42:00 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(DecimalToBinary.convert(10));
+        System.out.println(DecimalToBinary.convert(100));
+        System.out.println(DecimalToBinary.convert(1234));
+//        System.out.println(DecimalToBinary.convert(-15)); // Exception
+        System.out.println(DecimalToBinary.convert(0));
+    }
+}
